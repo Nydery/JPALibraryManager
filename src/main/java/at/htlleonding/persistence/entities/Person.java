9f0540a901534eef65d44637a01184523673f914
Simon Rausch-Schott <simon.rausch-schott@24speed.at>
2022-03-22 09:51:34 +0100

    Add MappedSuperclass Person

---
+++ src/main/java/at/htlleonding/persistence/entities/Person.java
@@ -0,0 +1,49 @@
+package at.htlleonding.persistence.entities;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+
+    //--------------------------------
+
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    //--------------------------------
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
