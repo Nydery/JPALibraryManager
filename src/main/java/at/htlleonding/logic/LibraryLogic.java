cdf5f4c67d2ff06d0540917c3029c0aaa5c3a34e
Simon Rausch-Schott <simon.rausch-schott@24speed.at>
2022-04-19 10:27:28 +0200

    Comment out add() and remove() methods

--- src/main/java/at/htlleonding/logic/LibraryLogic.java
+++ src/main/java/at/htlleonding/logic/LibraryLogic.java
@@ -8,13 +8,13 @@ import javax.inject.Inject;
  LibraryLogic: Interface to the library data. Provides an entity-free interface to the outside.
  Responisibilities: Takes data from outside (e.g. via strings) and creates or updates the entities in the repository
  Collaborations: Uses LibraryRepository to persist and retrieve entities
-
  */
 @ApplicationScoped
 public class LibraryLogic {
     @Inject
     LibraryRepository repository;
 
+    /*
     public void add(IEntity entity) {
         repository.add(entity);
     }
@@ -22,4 +22,5 @@ public class LibraryLogic {
     public void remove(IEntity entity) {
         repository.remove(entity);
     }
+    */
 }
